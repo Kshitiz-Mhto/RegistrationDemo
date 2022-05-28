ad52ff965e671d2154a231cb6f57c3f0461e28a5
ibergx00 <ibergx00@localhost.localdomain>
2022-05-28 16:10:23 +0545

    1 piece of 3

---
+++ src/main/java/com/register/Registration.java
@@ -0,0 +1,101 @@
+package com.register;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Registration extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Registration</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            //fetching all the (inputed)details of user
+            String name = request.getParameter("user_name");
+            String email = request.getParameter("user_email");
+            String password = request.getParameter("user_pass");
+            //connecting to the database
+            try{
+                //loading the driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+                //establishing connection
+                String url = "jdbc:mysql://localhost:3306/STUDENTS?zeroDateTimeBehavior=CONVERT_TO_NULL";
+                String usr = "root";
+                String pass = "Ibergx00@stepup";
+                //Establishing connection
+               try (Connection connect=DriverManager.getConnection(url,usr,pass)){
+                    // query
+                    String q = "insert into students(student_name,email,password) values(?,?,?)";
+                  //enabling to send SQL commands to DB
+                    try(PreparedStatement pst = connect.prepareStatement(q)){
+                    pst.setString(1,name);
+                    pst.setString(2,email);
+                    pst.setString(3,password);
+                    
+                    pst.executeUpdate();
+                    out.println("done...");
+                    }
+                    connect.close();
+               }
+            } catch (ClassNotFoundException | SQLException e){
+                out.println(e.getMessage() + "<h1>error ...</h1>");
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
