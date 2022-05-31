fc5b26a524d332f70ade0712c2bcc7acdc3a769c
ibergx00 <ibergx00@localhost.localdomain>
2022-05-31 22:08:31 +0545

    2 of 3

--- src/main/java/com/register/Registration.java
+++ src/main/java/com/register/Registration.java
@@ -17,12 +17,6 @@ public class Registration extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try ( PrintWriter out = response.getWriter()) {
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Registration</title>");            
-            out.println("</head>");
-            out.println("<body>");
             //fetching all the (inputed)details of user
             String name = request.getParameter("user_name");
             String email = request.getParameter("user_email");
@@ -39,7 +33,7 @@ public class Registration extends HttpServlet {
                 //Establishing connection
                try (Connection connect=DriverManager.getConnection(url,usr,pass)){
                     // query
-                    String q = "insert into students(student_name,email,password) values(?,?,?)";
+                    String q = "insert into students(name,email,password) values(?,?,?)";
                   //enabling to send SQL commands to DB
                     try(PreparedStatement pst = connect.prepareStatement(q)){
                     pst.setString(1,name);
@@ -54,48 +48,19 @@ public class Registration extends HttpServlet {
             } catch (ClassNotFoundException | SQLException e){
                 out.println(e.getMessage() + "<h1>error ...</h1>");
             }
-            out.println("</body>");
-            out.println("</html>");
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
